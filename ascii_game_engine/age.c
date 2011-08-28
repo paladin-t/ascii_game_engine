cc18ff0ecbdb714082ab3144b7abfcc6253cc71d
hellotony521@gmail.com <hellotony521@gmail.com@9bec9c06-d637-7093-fc36-bc1506f7fa44>
2011-08-28 23:42:43 +0000

    common commit

--- ascii_game_engine/age.c
+++ ascii_game_engine/age.c
@@ -154,8 +154,8 @@ s32 run_world(void) {
 		elapsed = now - old;
 		old = now;
 		update_sound(AGE_SND, elapsed);
-		update_canvas(AGE_CVS, elapsed);
 		render_canvas(AGE_CVS, elapsed);
+		update_canvas(AGE_CVS, elapsed);
 		delay = EXPECTED_FRAME_TIME - (age_tick_count() - old);
 		if(delay > 0) {
 			age_sleep(delay);
